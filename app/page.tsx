ae4a434442e6fc7e0c3bf26708be79c0174c9555
PsylanL <dylan_lopez82201@elpoli.edu.co>
2025-11-19 18:55:10 -0500

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 const Home = () => {
   return (
-    <div>
+    <>
       <h1 className="h1-bold">Welcome home</h1>
-      <h2>next</h2>
-    </div>
+      <h1 className="h1-bold font-space-grotesk">
+        Welcome home (SPACE GROTESK)
+      </h1>
+    </>
   );
 };
 
