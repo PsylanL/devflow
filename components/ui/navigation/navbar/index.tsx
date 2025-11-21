6718a0d826c2b9e6f90bf9cf25767d1ca9aa4c29
PsylanL <dylan_lopez82201@elpoli.edu.co>
2025-11-20 20:51:52 -0500

    Create a theme toggler

--- components/ui/navigation/navbar/index.tsx
+++ components/ui/navigation/navbar/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Theme from "./Theme";
 
 const Navbar: React.FC = () => {
   return (
@@ -18,7 +19,9 @@ const Navbar: React.FC = () => {
         </p>
       </Link>
       <p>Global Search</p>
-      <div className="flex-between gap-5">Theme</div>
+      <div className="flex-between gap-5">
+        <Theme />
+      </div>
     </nav>
   );
 };
