4ac1e952aab19ab750c879214dc686919c6e9d60
PsylanL <dylan_lopez82201@elpoli.edu.co>
2025-11-20 21:26:35 -0500

    Defined (root) and (auth) structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "@/components/ui/navigation/navbar";
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main>
+      <Navbar />
+      {children}
+    </main>
+  );
+};
+
+export default RootLayout;
